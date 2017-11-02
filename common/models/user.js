f41d0351e6c5e645ccd3fcd0065f6b09dc1f6abc
manishpatil159 <manish.patil159@gmail.com>
2017-11-02 14:58:47 +0530

    update to send permissions with user

--- common/models/user.js
+++ common/models/user.js
@@ -8,9 +8,9 @@
 var config = require('../../server/config.json');
 var path = require('path');
 
-module.exports = function(User) {
+module.exports = function (User) {
   // send verification email after registration
-  User.afterRemote('create', function(context, user, next) {
+  User.afterRemote('create', function (context, user, next) {
     var options = {
       type: 'email',
       to: user.email,
@@ -21,7 +21,7 @@ module.exports = function(User) {
       user: user,
     };
 
-    user.verify(options, function(err, response) {
+    user.verify(options, function (err, response) {
       if (err) {
         User.deleteById(user.id);
         return next(err);
@@ -29,7 +29,7 @@ module.exports = function(User) {
       context.res.render('response', {
         title: 'Signed up successfully',
         content: 'Please check your email and click on the verification link ' +
-            'before logging in.',
+        'before logging in.',
         redirectTo: '/',
         redirectToLinkText: 'Log in',
       });
@@ -37,36 +37,36 @@ module.exports = function(User) {
   });
 
   // Method to render
-  User.afterRemote('prototype.verify', function(context, user, next) {
+  User.afterRemote('prototype.verify', function (context, user, next) {
     context.res.render('response', {
       title: 'A Link to reverify your identity has been sent ' +
-        'to your email successfully',
+      'to your email successfully',
       content: 'Please check your email and click on the verification link ' +
-        'before logging in',
+      'before logging in',
       redirectTo: '/',
       redirectToLinkText: 'Log in',
     });
   });
 
   // send password reset link when requested
-  User.on('resetPasswordRequest', function(info) {
+  User.on('resetPasswordRequest', function (info) {
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html = 'Click <a href="' + url + '?access_token=' +
-        info.accessToken.id + '">here</a> to reset your password';
+      info.accessToken.id + '">here</a> to reset your password';
 
     User.app.models.Email.send({
       to: info.email,
       from: info.email,
       subject: 'Password reset',
       html: html,
-    }, function(err) {
+    }, function (err) {
       if (err) return console.log('> error sending password reset email');
       console.log('> sending password reset email to:', info.email);
     });
   });
 
   // render UI page after password change
-  User.afterRemote('changePassword', function(context, user, next) {
+  User.afterRemote('changePassword', function (context, user, next) {
     context.res.render('response', {
       title: 'Password changed successfully',
       content: 'Please login again with new password',
@@ -76,12 +76,30 @@ module.exports = function(User) {
   });
 
   // render UI page after password reset
-  User.afterRemote('setPassword', function(context, user, next) {
+  User.afterRemote('setPassword', function (context, user, next) {
     context.res.render('response', {
       title: 'Password reset success',
       content: 'Your password has been reset successfully',
       redirectTo: '/',
       redirectToLinkText: 'Log in',
     });
+  });
+
+  User.afterRemote('findById', function (context, user, next) {
+    var role = user.roles();
+    if (role != undefined && role.length > 0) {
+      var permissions = [];
+      User.app.models.Permission.find({ where: { principalId: role[0].name } }, function (err, p) {
+        user.permissions = p;
+        next();
+      });
+      // User.app.models.ACL.find({ where: { principalId: role[0].name } }, function (err, p) {
+      //   user.permissions = p;
+      //   next();
+      // });
+    }
+    else {
+      next();
+    }
   });
 };
