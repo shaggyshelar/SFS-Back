cc85e49945dfaf5bfffbea8e61b0c32e64550b01
manishpatil159 <manish.patil159@gmail.com>
2017-11-04 14:24:20 +0530

    added menus and permissions in sample data

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -9,6 +9,8 @@ module.exports = function (app) {
   var TempModel = app.models.TempModel;
   var Acl = app.models.ACL;
   var FeatureList = app.models.FeatureList;
+  var Menu = app.models.Menu;
+  var PermissionList = app.models.PermissionList;
 
   User.create([
     { username: 'John', email: 'john@doe.com', password: 'opensesame', 'emailVerified': true, 'isPasswordChanged': false },
@@ -60,63 +62,64 @@ module.exports = function (app) {
         console.log('Created principal:', principal);
 
         //assign permissions to bob
-        var permissionOptions = [{
-          "permission": "School.Read",
-          "principalName": "admin",
-          "principalId": "1"
-        }, {
-          "permission": "School.Create",
-          "principalName": "admin",
-          "principalId": "1"
-        },
-        {
-          "permission": "School.Update",
-          "principalName": "admin",
-          "principalId": "1"
-        },
-        {
-          "permission": "School.Delete",
-          "principalName": "admin",
-          "principalId": "1"
-        }, {
-          "permission": "user.Read",
-          "principalName": "admin",
-          "principalId": "1"
-        }, {
-          "permission": "user.Create",
-          "principalName": "admin",
-          "principalId": "1"
-        },
-        {
-          "permission": "user.Update",
-          "principalName": "admin",
-          "principalId": "1"
-        },
-        {
-          "permission": "user.Delete",
-          "principalName": "admin",
-          "principalId": "1"
-        }, {
-          "permission": "TempModel.Read",
-          "principalName": "admin",
-          "principalId": "1"
-        }, {
-          "permission": "TempModel.Create",
-          "principalName": "admin",
-          "principalId": "1"
-        },
-        {
-          "permission": "TempModel.Update",
-          "principalName": "admin",
-          "principalId": "1"
-        },
-        {
-          "permission": "TempModel.Delete",
-          "principalName": "admin",
-          "principalId": "1"
-        }];
+        var permissionOptions = [
+          {
+            "permission": "School.Read",
+            "principalName": "admin",
+            "principalId": "1"
+          }, {
+            "permission": "School.Create",
+            "principalName": "admin",
+            "principalId": "1"
+          },
+          {
+            "permission": "School.Update",
+            "principalName": "admin",
+            "principalId": "1"
+          },
+          {
+            "permission": "School.Delete",
+            "principalName": "admin",
+            "principalId": "1"
+          }, {
+            "permission": "user.Read",
+            "principalName": "admin",
+            "principalId": "1"
+          }, {
+            "permission": "user.Create",
+            "principalName": "admin",
+            "principalId": "1"
+          },
+          {
+            "permission": "user.Update",
+            "principalName": "admin",
+            "principalId": "1"
+          },
+          {
+            "permission": "user.Delete",
+            "principalName": "admin",
+            "principalId": "1"
+          }, {
+            "permission": "TempModel.Read",
+            "principalName": "admin",
+            "principalId": "1"
+          }, {
+            "permission": "TempModel.Create",
+            "principalName": "admin",
+            "principalId": "1"
+          },
+          {
+            "permission": "TempModel.Update",
+            "principalName": "admin",
+            "principalId": "1"
+          },
+          {
+            "permission": "TempModel.Delete",
+            "principalName": "admin",
+            "principalId": "1"
+          }];
         Permission.create(permissionOptions, function (err, per) {
-          if(err) throw err;
+          if (err) throw err;
           console.log('permissions added');
         });
       });
@@ -130,7 +133,7 @@ module.exports = function (app) {
         "permission": "DENY",
         "principalType": "ROLE",
         "principalId": "$everyone",
-      }, 
+      },
       {
         "model": "user",
         "accessType": "*",
@@ -239,27 +242,118 @@ module.exports = function (app) {
       console.log("Acl added to deny TempModel access all");
     });
 
-    var features = [{
-      "FeatureName": "User",
-      "ModelName": "user"
-    }, {
-      "FeatureName": "Role",
-      "ModelName": "role"
-    }, {
-      "FeatureName": "Fee",
-      "ModelName": "Fee"
-    }, {
-      "FeatureName": "Student",
-      "ModelName": "Student"
-    }, {
-      "FeatureName": "Temp",
-      "ModelName": "TempModel"
-    }];
+    var features = [
+      {
+        "FeatureName": "User",
+        "ModelName": "user"
+      }, {
+        "FeatureName": "Role",
+        "ModelName": "role"
+      }, {
+        "FeatureName": "Fee",
+        "ModelName": "Fee"
+      }, {
+        "FeatureName": "Student",
+        "ModelName": "Student"
+      }, {
+        "FeatureName": "Temp",
+        "ModelName": "TempModel"
+      }];
     features.map(function (f, i) {
       FeatureList.create(f, function () {
         console.log("Added feature " + f.FeatureName);
       });
     });
+
+    var menus = [
+      {
+        "menuName": "Institute",
+        "menuParentId": 0,
+        "logicalSequence": 1,
+        "isDelete": false
+      },
+      {
+        "menuName": "Board",
+        "menuParentId": 0,
+        "logicalSequence": 2,
+        "isDelete": false
+      },
+      {
+        "menuName": "Schools",
+        "menuParentId": 0,
+        "logicalSequence": 3,
+        "isDelete": false
+      }
+    ];
+    var menuPermissions = [
+      {
+        "menuId": 1,
+        "permissionName": "Institute.Create",
+        "isDelete": false
+      },
+      {
+        "menuId": 1,
+        "permissionName": "Institute.Read",
+        "isDelete": false
+      },
+      {
+        "menuId": 1,
+        "permissionName": "Institute.Update",
+        "isDelete": false
+      },
+      {
+        "menuId": 1,
+        "permissionName": "Institute.Delete",
+        "isDelete": false
+      },
+      {
+        "menuId": 2,
+        "permissionName": "Board.Create",
+        "isDelete": false
+      },
+      {
+        "menuId": 2,
+        "permissionName": "Board.Read",
+        "isDelete": false
+      },
+      {
+        "menuId": 2,
+        "permissionName": "Board.Update",
+        "isDelete": false
+      },
+      {
+        "menuId": 2,
+        "permissionName": "Board.Delete",
+        "isDelete": false
+      },
+      {
+        "menuId": 3,
+        "permissionName": "School.Create",
+        "isDelete": false
+      },
+      {
+        "menuId": 3,
+        "permissionName": "School.Read",
+        "isDelete": false
+      },
+      {
+        "menuId": 3,
+        "permissionName": "School.Update",
+        "isDelete": false
+      },
+      {
+        "menuId": 3,
+        "permissionName": "School.Delete",
+        "isDelete": false
+      }
+    ];
+    Menu.create(menus, function (err, data) {
+      if (err) throw err;
+      PermissionList.create(menuPermissions, function (err, per) {
+        if (err) throw err;
+      });
+    });
+
 
   });
 };
