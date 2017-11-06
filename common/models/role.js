f42f1bd6d90808a6d300f44923c642d953e799a9
manishpatil159 <manish.patil159@gmail.com>
2017-11-06 23:51:46 +0530

    updated roles to retun permissions with findById

--- common/models/role.js
+++ common/models/role.js
@@ -1,4 +1,41 @@
 'use strict';
-module.exports = function(role) {
-	
+var app = require('../../server/server');
+
+module.exports = function (role) {
+    role.afterRemote('findById', function (context, role, next) {
+
+        app.models.RolepermissionDetails.find({ where: { roleId: role.id } }, function (err, permissions) {
+            if (permissions && permissions.length > 0) {
+                role.permissions = permissions;
+
+                var query = [];
+                permissions.map(function (p, i) {
+                    var permArr = p.permissionName.split('.');
+                    if (permArr && permArr.length == 2) {
+                        query.push({ modelName: permArr[0] });
+                    }
+                });
+                app.models.Permission.find({ and: query }, function (err, features) {
+                    permissions.map(function (p, i) {
+                        var permArr = p.permissionName.split('.');
+                        if (permArr && permArr.length == 2) {
+                            var fo = features.find(function (f) {
+                                return permArr[0] == f.modelName;
+                            });
+                            if (fo) {
+                                p.featureName = fo.permissionName;
+                            }
+                        }
+
+                    });
+
+                    next();
+
+                });
+            }
+            else {
+                next();
+            }
+        });
+    });
 };
