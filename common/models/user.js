e2de51ffff73d043916a24e9285c9aac9ea7e72c
manishpatil159 <manish.patil159@gmail.com>
2017-11-17 12:55:02 +0530

    updates to user locking functionality

--- common/models/user.js
+++ common/models/user.js
@@ -57,15 +57,25 @@ module.exports = function (User) {
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html = 'Click <a href="' + url + '?access_token=' +
       info.accessToken.id + '">here</a> to reset your password';
+    var subject = "Password reset";
 
-    User.app.models.Email.send({
-      to: info.email,
-      from: info.email,
-      subject: 'Password reset',
-      html: html,
-    }, function (err) {
-      if (err) return console.log('> error sending password reset email');
-      console.log('> sending password reset email to:', info.email);
+    app.models.user.findOne({ where: { email: info.email } }, function (err, _user) {
+      if (err) throw err;
+      if (_user.isBolocked) {
+        html = 'Click <a href="' + url + '?access_token=' +
+          info.accessToken.id + '">here</a> to reset your password and unlock your account.';
+        subject = "Unlock account";
+      }
+
+      User.app.models.Email.send({
+        to: info.email,
+        from: info.email,
+        subject: subject,
+        html: html,
+      }, function (err) {
+        if (err) return console.log('> error sending password reset email');
+        console.log('> sending password reset email to:', info.email);
+      });
     });
   });
 
