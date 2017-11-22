613d4e5f6b216b1f93176e2755337e6556ee4251
manishpatil159 <manish.patil159@gmail.com>
2017-11-22 18:48:38 +0530

    updated functionality to check if fee plan associations with same class and category exist for same academic year

--- common/models/Feeplanassociation.js
+++ common/models/Feeplanassociation.js
@@ -2,20 +2,45 @@
 module.exports = function (Feeplanassociation) {
 
     Feeplanassociation.updateFeeplanAssociation = function (feeplanassociations, options, cb) {
-        var conditions = [];
 
         if (feeplanassociations && feeplanassociations.length > 0) {
 
+            var searchConditions = [];
             feeplanassociations.map(function (assocn, index) {
-                conditions.push({ feeplanId: assocn.feeplanId, academicYear: assocn.academicYear });
+                searchConditions.push({ categoryId: assocn.categoryId, classId: assocn.classId, academicYear: assocn.academicYear });
             });
+            Feeplanassociation.find({ where: { or: searchConditions }, include: { relation: "FeeplanassociationFeeplan" } }, function (err, duplicateAssoc) {
+                if (duplicateAssoc && duplicateAssoc.length > 0) {
+                    var errorFeePlans = [];
+                    var error = new Error();
+                    error.status = 422;
+                    error.message = "A combination for class and category already exist for ";
+                    duplicateAssoc.map(function (errorRecords, index) {
+                        if (errorFeePlans.indexOf(errorRecords.__data.FeeplanassociationFeeplan.feePlanName) == -1) {
+                            errorFeePlans.push(errorRecords.__data.FeeplanassociationFeeplan.feePlanName);
+                        }
+                    });
+                    errorFeePlans.map(function (errorPlan, index) {
+                        error.message += errorPlan;
+                        if (errorFeePlans.length != (index + 1))
+                            error.message += ", "
+                    });
+                    cb(error)
+                }
+                else {
+                    var conditions = [];
+                    feeplanassociations.map(function (assocn, index) {
+                        conditions.push({ feeplanId: assocn.feeplanId, academicYear: assocn.academicYear });
+                    });
 
-            Feeplanassociation.destroyAll({ or: conditions }, function (err, info) {
-                if (err) throw err;
-                Feeplanassociation.create(feeplanassociations, function (err, savedAssociations) {
-                    if (err) throw err;
-                    cb(null, savedAssociations);
-                });
+                    Feeplanassociation.destroyAll({ or: conditions }, function (err, info) {
+                        if (err) throw err;
+                        Feeplanassociation.create(feeplanassociations, function (err, savedAssociations) {
+                            if (err) throw err;
+                            cb(null, savedAssociations);
+                        });
+                    });
+                }
             });
         }
         else {
