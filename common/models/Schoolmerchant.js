a4247feef572612e4a757239da76af5a9af5ac63
manishpatil159 <manish.patil159@gmail.com>
2017-11-24 16:38:45 +0530

    implemented functionality so that there will be one default merchant for a particular school.

--- common/models/Schoolmerchant.js
+++ common/models/Schoolmerchant.js
@@ -1,4 +1,30 @@
 'use strict';
-module.exports = function(Schoolmerchant) {
-	
+module.exports = function (Schoolmerchant) {
+
+    Schoolmerchant.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            if (ctx.instance.isDefault && ctx.instance.schoolId != undefined) {
+                Schoolmerchant.updateAll({ schoolId: ctx.instance.schoolId, isDefault: true }, { isDefault: false }, function (err, updatedMerchants) {
+                    if (err) throw err;
+                    next();
+                });
+            }
+            else {
+                next();
+            }
+        }
+        else if (ctx.data) {
+            if (ctx.data.isDefault && ctx.data.schoolId !== undefined) {
+                Schoolmerchant.updateAll({ schoolId: ctx.data.schoolId, isDefault: true }, { isDefault: false }, function (err, updatedMerchants) {
+                    if (err) throw err;
+                    next();
+                });
+            }
+            else {
+                next();
+            }
+        }
+        next();
+    });
+
 };
