e0d1372494110411e6838444a786fa9b4e4f15d5
manishpatil159 <manish.patil159@gmail.com>
2017-11-30 17:55:46 +0530

    bug fix: application crashes while creating feeplan associations

--- common/models/Feeplanassociation.js
+++ common/models/Feeplanassociation.js
@@ -10,7 +10,7 @@ module.exports = function (Feeplanassociation) {
             feeplanassociations.map(function (assocn, index) {
                 searchConditions.push({ feeplanId: { "neq": assocn.feeplanId }, categoryId: assocn.categoryId, classId: assocn.classId, academicYear: assocn.academicYear });
             });
-            Feeplanassociation.find({ where: { or: searchConditions }, include: { relation: "FeeplanassociationFeeplan" } }, function (err, duplicateAssoc) {
+            Feeplanassociation.find({ where: { or: searchConditions }, include: ["FeeplanassociationFeeplan", "FeeplanassociationClass"] }, function (err, duplicateAssoc) {
                 if (duplicateAssoc && duplicateAssoc.length > 0) {
                     var errorFeePlans = [];
                     var error = new Error();
