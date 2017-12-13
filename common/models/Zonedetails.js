0116a20cd40e155cc349dfee76364e6825e6161b
manishpatil159 <manish.patil159@gmail.com>
2017-12-13 15:15:59 +0530

    Updated validations to check if _zonedetails is not empty. (All Zonedetails were deleted when frequency was updated)

--- common/models/Zonedetails.js
+++ common/models/Zonedetails.js
@@ -1,24 +1,29 @@
 'use strict';
 module.exports = function (Zonedetails) {
     Zonedetails.updateZonedetails = function (_zonedetails, options, cb) {
-        var conditions = [];
-        _zonedetails.map(function (z, i) {
-            conditions.push({ zoneId: z.zoneId });
-        });
+        if (_zonedetails && _zonedetails.length > 0) {
+            var conditions = [];
+            _zonedetails.map(function (z, i) {
+                conditions.push({ zoneId: z.zoneId });
+            });
 
-        Zonedetails.destroyAll({ or: conditions }, function (err, info) {
-            if (err) {
-                cb(err);
-            }
-            else {
-                Zonedetails.create(_zonedetails, function (err, savedZonedetails) {
-                    if (err)
-                        cb(err);
-                    else
-                        cb(null, savedZonedetails);
-                });
-            }
-        });
+            Zonedetails.destroyAll({ or: conditions }, function (err, info) {
+                if (err) {
+                    cb(err);
+                }
+                else {
+                    Zonedetails.create(_zonedetails, function (err, savedZonedetails) {
+                        if (err)
+                            cb(err);
+                        else
+                            cb(null, savedZonedetails);
+                    });
+                }
+            });
+        }
+        else {
+            cb(null);
+        }
     }
 
     Zonedetails.remoteMethod('updateZonedetails', {
