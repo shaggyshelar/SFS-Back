d24aa56a068b1b75299e6c3d93f95c010a163560
manishpatil159 <manish.patil159@gmail.com>
2017-12-13 16:33:38 +0530

    Fixed timezone related issue. (added 5:30 hours to current time so that date adn time is saved as IST i database)

---
+++ common/shared/dateHelper.js
@@ -0,0 +1,15 @@
+var configFilePath = process.env.NODE_ENV == undefined ?
+    '' : '.' + process.env.NODE_ENV;
+var config = require('../../server/config' + configFilePath + '.json');
+
+var dateHelper = function () {
+};
+
+dateHelper.getUTCManagedDateTime = function () {
+    var localeDate = new Date();
+    localeDate.setHours(localeDate.getHours() + config.hourDifferenceUTC);
+    localeDate.setMinutes(localeDate.getMinutes() + config.minutesDifferenceUTC);
+    return localeDate;
+}
+
+module.exports = dateHelper;
