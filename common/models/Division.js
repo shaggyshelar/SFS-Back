9a7a7d1f7380804884a1b0004da3fa3ac4eaa19c
Manish Patil <manish.patil@Eternus.local>
2018-04-09 15:58:08 +0530

    updates to not show divisions for deleted classes.

--- common/models/Division.js
+++ common/models/Division.js
@@ -1,3 +1,14 @@
 'use strict';
 module.exports = function(Division) {
+   Division.afterRemote('find', function (context, _division, next) {
+        if(_division) {
+            var _division = _division.filter(function(item) {
+                if(item.__data && item.__data.DivisionClass && item.__data.DivisionSchool) {
+                    return item;
+                }
+            });
+        } 
+        context.result = _division;
+        next();
+      });
 };
